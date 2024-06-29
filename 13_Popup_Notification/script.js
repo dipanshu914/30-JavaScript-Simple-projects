18a9d38757be8f95fbc72607482d15cf05378408
dipanshu914 <122882167+dipanshu914@users.noreply.github.com>
2024-06-29 18:23:46 +0530

    adding final commit message to popup-Notification

---
+++ 13_Popup_Notification/script.js
@@ -0,0 +1,9 @@
+const Popup = document.getElementById("popup");
+
+function openPopup() {
+	Popup.classList.add("open-popup");
+}
+
+function closePopup() {
+	Popup.classList.remove("open-popup");
+}
